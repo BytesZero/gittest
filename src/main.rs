40a27fc526ac16fcd2e0fb58065eaad16e57f240
Zero <1300326388@qq.com>
2024-06-16 18:57:27 +0800

    add 5

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     println!("Hello, world2!");
     println!("Hello, world3!");
     println!("Hello, world4!");
+    println!("Hello, world5!");
     println!("Hello, world6!");
     println!("Hello, world7!");
 }
